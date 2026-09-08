e5bc04d17ee645c9fe865c11399241ebcc52fa13
Mael Albany <maelalbany0@gmail.com>
2026-09-09 00:24:11 +0200

    feat: add ThemeSwitcher component and update footer layout

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,14 @@
+import ThemeSwitcher from './themeSwitcher.jsx';
+
 function Footer() {
     return (
         <footer className="footer">
+            <div className="footer-content">
+            <ThemeSwitcher />
+
             <div className="card-footer">
                 <p>&copy; 2026 Mael Albany. All rights reserved.</p>
+            </div>
             </div>
         </footer>
     );
